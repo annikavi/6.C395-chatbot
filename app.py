08e104e885a6d1eff9e9ae691c782a6ab3ac210a
Sasha Krigel <sasha.krigel@gmail.com>
2026-03-08 17:22:26 -0400

    inserting response generation from src/chat.py into app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def create_chatbot():
                 - Return that response as a string
         """
         # TODO: Generate and return response
-        pass
+        return chatbot.get_response(message, history)
 
     
     
